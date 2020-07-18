e41d2c5678ec93f207a38cdf7593582a2cbc064f
caroline-swartz <carolineswartzz@gmail.com>
2020-07-17 19:48:44 -0700

    created users table in db, made sign up page work

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home"
+import SignUp from "../components/SignUp";
+import Users from "../components/Users";
 
 export default (
     <Router>
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/signup" exact component={SignUp} />
+            <Route path="/users" exact component={Users} />
         </Switch>
     </Router>
 );
