fe4b361b16e4fdcc5979172418bb6de13f966ba0
Kyle Giannini <kylgia@gmail.com>
2020-07-20 14:39:08 -0700

    fixed home front end

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -21,10 +21,14 @@ class Home extends React.Component {
         return String(str).replace(/</g, "&lt;").replace(/>/g, "&gt;");
     }
 
+    //updates state variables when user types in a field
     onChange(event) {
         this.setState({ [event.target.name]: event.target.value });
     }
 
+    //TODO add login form submission event handler
+
+    //handles submission event from sign up form
     onSignupSubmit(event) {
         event.preventDefault();
         const url = "/users/create";
@@ -59,48 +63,46 @@ class Home extends React.Component {
         .catch(error => console.log(error.message));
     }
 
+    //the login form to be displayed 
     showLogin = () => {
         document.getElementById("topLine").style.visibility = "hidden";
         return(
-            <div>
+            <div classname="home-form-div">
                 <hr className="my-4"/>
-                <div className="offset-lg-4">
-                    <form className="w-50">
-                        <div className="form-group">
-                            <input type="text" name="email" id="userEmail" className="form-control" required placeholder="Email"/>
-                        </div>
-                            <div className="form-group">
-                                <input type="text" name="password" id="userPassword" className="form-control" required placeholder="Password"/>
-                        </div>
-                        <button type="submit" className="btn custom-button3">Log In</button>
-                    </form>
-                </div>
+                <form className="home-form">
+                    <div className="form-group">
+                        <input type="text" name="email" id="userEmail" className="form-control" required placeholder="Email"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="password" id="userPassword" className="form-control" required placeholder="Password"/>
+                    </div>
+                    <button type="submit" className="btn custom-button3">Log In</button>
+                </form>
             </div>
         );
     }
 
+    //the sign up form to be displayed
     showSignUp = () => {
         document.getElementById("topLine").style.visibility = "hidden";
         return(
-            <div>
+            <div classname="home-form-div">
                 <hr className="my-4"/>
-                <div className="offset-lg-4">
-                    <form className="w-50" onSubmit={this.onSignupSubmit}>
-                        <div className="form-group">
-                            <input type="text" name="firstname" id="userFirstName" className="form-control" required placeholder="First Name" onChange={this.onChange}/>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" name="lastname" id="userLastName" className="form-control" required placeholder="Last Name" onChange={this.onChange}/>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" name="email" id="userEmail" className="form-control" required placeholder="Email" onChange={this.onChange}/>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" name="password_digest" id="userPasswordDigest" className="form-control" required placeholder="Password" onChange={this.onChange}/>
-                        </div>
-                        <button type="submit" className="btn custom-button3">Sign Up</button>
-                    </form>
-                </div>
+                <form className="home-form" onSubmit={this.onSignupSubmit}>
+                    <div className="form-group">
+                        <input type="text" name="firstname" id="userFirstName" className="form-control" required placeholder="First Name" onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="lastname" id="userLastName" className="form-control" required placeholder="Last Name" onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="email" id="userEmail" className="form-control" required placeholder="Email" onChange={this.onChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="password_digest" id="userPasswordDigest" className="form-control" required placeholder="Password" onChange={this.onChange}/>
+                    </div>
+                    <button type="submit" className="btn custom-button3">Sign Up</button>
+                </form>
             </div>
         )
     }
@@ -109,7 +111,7 @@ class Home extends React.Component {
         return (
             <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center home-background">
                 <div className="jumbotron jumbotron-fluid bg-transparent">
-                    <div className="container primary-color border border-secondary rounded border-padding">
+                    <div className="container primary-color border border-secondary rounded border-padding text-center">
                         <h1 className="display-4">Welcome to Trip Planner</h1>
                         <hr id="topLine" className="my-4"/>
                         <div className="d-flex align-items-center justify-content-center">
