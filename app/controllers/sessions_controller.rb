407a51fa6290d04799a40e445e944111e7133fd7
caroline-swartz <carolineswartzz@gmail.com>
2020-07-21 13:28:23 -0700

    added comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(email: session_params[:email])
 
+        #checks if the user exists and verifies the email and password combination
         if @user && @user.authenticate(session_params[:password])
             login!
             render json: {
