78b7a642e8e1266f0576569b4cbbc8194dc2ca9c
caroline-swartz <carolineswartzz@gmail.com>
2020-08-14 14:54:12 -0700

    display trip info and fix date format

--- app/javascript/components/Trip.jsx
+++ app/javascript/components/Trip.jsx
@@ -11,8 +11,10 @@ class Trip extends React.Component {
 
     render() {
         return (
-            <div className="primary-color d-flex align-items-center justify-content-center">
+            <div className="primary-color d-flex flex-column align-items-center justify-content-center">
                 <h1>{this.props.currentTrip.name}</h1>
+                <h2>{this.props.currentTrip.start.substring(5,7)}/{this.props.currentTrip.start.substring(8)}/{this.props.currentTrip.start.substring(0,4)} - {this.props.currentTrip.end.substring(5,7)}/{this.props.currentTrip.end.substring(8)}/{this.props.currentTrip.end.substring(0,4)}</h2>
+                <h2>Remaining budget: &#36;{this.props.currentTrip.budget}</h2>
             </div>
         );
     }
